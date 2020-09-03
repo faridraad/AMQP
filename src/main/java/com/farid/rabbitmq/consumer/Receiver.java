b001203a5f8a4c6bcb4066bffa3f997676f37dde
farid.raad@gmail.com <raadtech123!>
2020-09-03 13:41:18 +0430

    Add rabbitmq

---
+++ src/main/java/com/farid/rabbitmq/consumer/Receiver.java
@@ -0,0 +1,32 @@
+package com.farid.rabbitmq.consumer;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import com.rabbitmq.client.*;
+import java.util.concurrent.TimeoutException;
+
+public class Receiver {
+
+    private static final String QUEUE_NAME = "queue_name";
+
+    public static void main (String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope, AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
