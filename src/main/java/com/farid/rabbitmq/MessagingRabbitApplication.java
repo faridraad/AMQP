2586cc3e538dca31161f3d604cf512e43c68adaf
farid.raad@gmail.com <raadtech123!>
2020-09-27 15:44:50 +0330

    Create runner

---
+++ src/main/java/com/farid/rabbitmq/MessagingRabbitApplication.java
@@ -0,0 +1,11 @@
+package com.farid.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRabbitApplication {
+    public static void main(String[] args)  {
+        SpringApplication.run(MessagingRabbitApplication.class, args).close();
+    }
+}
